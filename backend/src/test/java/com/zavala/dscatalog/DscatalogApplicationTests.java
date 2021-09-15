0eed986298962f28b358bd2038b0fd090df6d939
Ramiro Arce <ramiroaz@gmail.com>
2021-09-15 17:45:19 -0300

    Backend project created

---
+++ backend/src/test/java/com/zavala/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zavala.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
