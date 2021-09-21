aadf473686b30bd6342419e857fe33faf46b35ea
Ramiro Arce <ramiroaz@gmail.com>
2021-09-21 17:17:48 -0300

    Find Category by Id; Exception handling

--- backend/src/main/java/com/zavala/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/zavala/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.zavala.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.zavala.dscatalog.dto.CategoryDTO;
 import com.zavala.dscatalog.entities.Category;
 import com.zavala.dscatalog.repositories.CategoryRepository;
+import com.zavala.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -24,6 +26,14 @@ public class CategoryService {
 				.stream()
 				.map(x -> new CategoryDTO(x))
 				.collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+	    Category entity = obj.orElseThrow( () -> new EntityNotFoundException("Entidade não encontrada"));
+	  //Category entity = obj.orElseThrow( () -> new ResourceNotFoundException("Entidade não encontrada"));
+		return new CategoryDTO(entity);
 	}
 	
 }
