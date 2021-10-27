aa8c06122381d9c7b4cf6bcdd02173fcc9e64d0e
Ramiro Arce <ramiroaz@gmail.com>
2021-10-27 13:40:10 -0300

    Basic Bean Validation

--- backend/src/main/java/com/zavala/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/zavala/dscatalog/dto/UserDTO.java
@@ -5,14 +5,20 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.zavala.dscatalog.entities.User;
 
 public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	@NotBlank(message = "Campo obrigatório")
 	private String firstName;
+	@NotBlank(message = "Campo obrigatório")
 	private String lastName;
+	@Email(message = "Favor informar e-mail válido")
 	private String email;
 	
 	Set<RoleDTO> roles = new HashSet<>();
