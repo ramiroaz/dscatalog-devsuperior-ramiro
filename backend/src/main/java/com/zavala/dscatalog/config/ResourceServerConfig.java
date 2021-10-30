e9faaa28176207ce8ac5be6534d8a6c2dd6b95a9
Ramiro Arce <ramiroaz@gmail.com>
2021-10-30 13:08:35 -0300

    H2 security configuration

--- backend/src/main/java/com/zavala/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/zavala/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.zavala.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -16,7 +19,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
-	private static final String[] PUBLIC = {"/oauth/token"};
+	@Autowired
+	private Environment env;		//ambiente de execução da aplicação
+	
+	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 	
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**" , "/categories/**" };
 	
@@ -37,6 +43,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		// Crud de produtos e categorias : usuário com perfil de Operador
 		// Crud de usuários : usuário com perfil de admim
 	
+		// se o profile ativo for "test", liberamos a rota da base de dados H2 
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();	
+		}
+		
 		http.authorizeRequests()
 			.antMatchers(PUBLIC).permitAll()
 			.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
